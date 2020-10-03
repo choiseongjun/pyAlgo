441e54a923589b02e7fa2271935eedc1632d793c
최성준 <csj2702@naver.com>
2020-10-03 17:11:25 +0900

    study

--- Book/해시테이블/leetcode347.py
+++ Book/해시테이블/leetcode347.py
@@ -1,15 +1,15 @@
-import collections
-
-def topKFrequent(nums,k):
-    a=collections.Counter(nums)
-    li=[]
-    for index,i in enumerate(a):
-        li.append(a[i])
-
-    for x in range(k):
-        print(li[x])
-
-nums=[1,1,1,2,2,3]
-k=2
-topKFrequent(nums,k)
-
+# import collections
+#
+# def topKFrequent(nums,k):
+#     a=collections.Counter(nums)
+#     li=[]
+#     for index,i in enumerate(a):
+#         li.append(a[i])
+#
+#     for x in range(k):
+#         print(li[x])
+#
+# nums=[1,1,1,2,2,3]
+# k=2
+# topKFrequent(nums,k)
+#
